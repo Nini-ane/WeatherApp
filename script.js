49161ec53e90169b44c770e8b2980d5f323bb1d9
Nini-ane <108152961+Nini-ane@users.noreply.github.com>
2022-06-30 21:16:26 +0200

    update from Sandbox
    copied over the progress from Code Sandbox

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-let weather = {
-  "paris": {
-    temp: 19.7,
-    humidity: 80
-  },
-  "tokyo": {
-    temp: 17.3,
-    humidity: 50
-  },
-  "lisbon": {
-    temp: 30.2,
-    humidity: 20
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  "moscow": {
-    temp: -5,
-    humidity: 20
-  }
-};
+let now = new Date();
 
-let city = prompt("What city are you from?");
+let today = document.querySelector("#date");
+console.log(today);
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+
+today.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function changeCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  document.getElementById("target-city").innerHTML = city.value;
+}
+
+let cityInput = document.querySelector("#city");
+cityInput.addEventListener("submit", changeCity);
+
+function showWeather(response) {
+  console.log(response);
+  let currentTemp = document.querySelector("#current-temp");
+  currentTemp.innerHTML = Math.round(response.data.main.temp) + "°C";
+}
+
+function getTemperature(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  let apiKey = "0225b3f0fd22f80ae48c36b7041e70ff";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(showWeather);
+  console.log(city.value);
+}
+
+cityInput.addEventListener("submit", getTemperature);
